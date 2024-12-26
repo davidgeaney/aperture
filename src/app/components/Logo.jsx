21c7a34dbf0aeeae46dfaa44f869426417046d61
David Geaney <davidgeaney16@gmail.com>
2024-12-26 15:57:21 +0000

    Initial commit

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center space-x-2 font-light">
+    <div className="flex items-center space-x-2 font-light">
       <div className="relative w-10 h-10">
         <div className="absolute inset-0 border-2 border-white rounded-full"></div>
         <div className="absolute inset-[6px] border border-white rounded-full"></div>
         <div className="absolute inset-[10px] bg-white rounded-full"></div>
       </div>
       <span className="text-xl tracking-[0.2em] uppercase">Aperture</span>
-    </Link>
+    </div>
   )
 }
 
